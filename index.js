3323b470d6711d3e2e9bd5c754cffde23a7f989b
Henry B Silva <henrybastoscomercial@gmail.com>
2024-04-28 23:13:45 -0300

    index.js file for export

---
+++ index.js
@@ -0,0 +1,11 @@
+import Draggy from './src/lib/Draggy.svelte';
+import DraggyItem from './src/lib/DraggyItem.svelte';
+import DraggyPlaceholder from './src/lib/DraggyPlaceholder.svelte';
+import DraggyVoid from './src/lib/DraggyVoid.svelte';
+
+export {
+   Draggy,
+   DraggyItem,
+   DraggyPlaceholder,
+   DraggyVoid  
+}
